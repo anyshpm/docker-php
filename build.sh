13e9317c01a15175d4f706da0d290e6e6aed3499
Anyshpm Chen <anyshpm@anyshpm.com>
2023-03-06 09:38:27 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ PHP_VERSION=8.0.25
 ALPINE_VERSION=alpine3.16
 TAG=$PHP_VERSION-fpm-$ALPINE_VERSION
 
-docker build --force-rm -t anyshpm/php:$TAG .
-#docker buildx build -t anyshpm/php:$TAG --platform linux/amd64,linux/arm64 --push .
+#docker build --force-rm -t anyshpm/php:$TAG .
+docker buildx build -t anyshpm/php:$TAG --platform linux/amd64,linux/arm64 --push .
